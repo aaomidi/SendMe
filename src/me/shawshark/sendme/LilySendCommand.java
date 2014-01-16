06944e2f5835833d364c447dc8505c715713a188
Chris <chrissbolding@gmail.com>
2014-01-17 09:28:06 +1100

    Update LilySendCommand.java

--- src/me/shawshark/sendme/LilySendCommand.java
+++ src/me/shawshark/sendme/LilySendCommand.java
@@ -14,20 +14,19 @@ public class LilySendCommand implements CommandExecutor {
 		this.m = m;
 	}
 	
-	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.RED + "Error: This command can only be run from in-game!");
 		} else {
 			Player p = (Player) sender;
+			if(!p.hasPermission("sendme.lilysend")) {
+				p.sendMessage(ChatColor.RED + "Error: You don't have permissions for this command.");
+			} else {
 				 if(args.length < 2) {
-					if(!p.hasPermission("sendme.lilysend")) {
-						p.sendMessage(ChatColor.RED + "Error: You don't have permissions for this command.");
-					} else {
-						p.sendMessage(ChatColor.GOLD + "Correct Usage: " + ChatColor.RED + "/lilysend [player] [servername]");
-					}
+					 p.sendMessage(ChatColor.GOLD + "Correct Usage: " + ChatColor.RED + "/lilysend [player] [servername]");
 				 } else {
+					 
 					 if(m.getServer().getPlayer(args[0]) !=null) {
 						 if(m.getServer().getPlayer(args[0]).isOnline()) {
 							 m.s.sendplayer(args[0], args[1]); 
@@ -36,8 +35,9 @@ public class LilySendCommand implements CommandExecutor {
 						 }
 					 } else {
 						 p.sendMessage(ChatColor.RED + "Error: " + args[0] + " Doesn't seem to be online!"); 
-					 }
-			} 
+					 } 
+				 }
+			}
 		 }
 		return false;
 	}
