9d50f8d9f96d83acbe674990a59e718ec7184994
Chris <chrissbolding@gmail.com>
2014-01-17 08:51:46 +1100

    Update ServerCommand.java

--- src/me/shawshark/sendme/ServerCommand.java
+++ src/me/shawshark/sendme/ServerCommand.java
@@ -1,12 +1,11 @@
 package me.shawshark.sendme;
 
-import lilypad.client.connect.api.request.RequestException;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class ServerCommand implements CommandExecutor {
 	
@@ -17,11 +16,11 @@ public class ServerCommand implements CommandExecutor {
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command command, String label, final String[] args) {
 		if(!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.RED + "Error: This command can only be run from in-game!");
 		} else {
-			Player p = (Player) sender;
+			final Player p = (Player) sender;
 				 if(args.length < 1) {
 					if(!p.hasPermission("sendme.use")) {
 						p.sendMessage(ChatColor.RED + "Error: You don't have permissions for this command.");
@@ -29,12 +28,19 @@ public class ServerCommand implements CommandExecutor {
 						p.sendMessage(ChatColor.GOLD + "Correct Usage: " + ChatColor.RED + "/server [servername]");
 					}
 				 } else {
-				 try {
-					m.s.sendplayer(p.getName(), args[0]);
-				} catch (RequestException e) {
-					p.sendMessage(ChatColor.GOLD + "Error: A error occurred!");
-					e.printStackTrace();
-				}
+					 if(m.timer < 1) {
+						 m.s.sendplayer(p.getName(), args[0]);
+					 } else {
+						 p.sendMessage(ChatColor.GOLD + "Teleportation will commence in " + m.timer + " seconds.");
+						 new BukkitRunnable() {
+							@Override
+							public void run() {
+								if(p.isOnline()) {
+									m.s.sendplayer(p.getName(), args[0]);
+								}
+							}
+						}.runTaskLater(m, m.timer * 20);
+					 }	
 			} 
 		 }
 		return false;
