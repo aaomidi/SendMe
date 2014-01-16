364c7ea452984b670e3de8b488aff9bff3d91d09
Chris <chrissbolding@gmail.com>
2014-01-17 07:52:46 +1100

    Update main.java

--- src/me/shawshark/sendme/main.java
+++ src/me/shawshark/sendme/main.java
@@ -3,40 +3,22 @@ package me.shawshark.sendme;
 import lilypad.client.connect.api.Connect;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
 	
-	public send s = new send(this);
-	
+	public Send s = new Send(this);
 	Connect connect;
 	
 	@Override
 	public void onEnable() {
         connect = Bukkit.getServer().getServicesManager().getRegistration(Connect.class).getProvider();
+        registercommands();
 	}
 	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {  
-		if(!(sender instanceof Player)) {
-			sender.sendMessage(ChatColor.RED + "Error: Commands can only be run from in-game!");
-		} else {
-			 if(cmd.getName().equalsIgnoreCase("server")) {
-				 Player p = (Player)sender;
-				 if(args.length < 1) {
-					if(!p.hasPermission("sendme.use")) {
-						p.sendMessage(ChatColor.RED + "Error: You don't have permissions for this command.");
-					} else {
-						p.sendMessage(ChatColor.GOLD + "Correct Usage: " + ChatColor.RED + "/server [servername]");
-					}
-				 } else {
-					 s.sendplayer(p, args[0]);
-				 } 
-			 }
-		 }
-		return false;
+	public void registercommands() {
+		getCommand("server").setExecutor(new ServerCommand(this));
+		getCommand("lilysend").setExecutor(new LilySendCommand(this));
+		System.out.print("[SendMe] Commands Registered!");
 	}
 }
